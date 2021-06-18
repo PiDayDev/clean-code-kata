4af5222198f40d02c3eb6cb44c2a7c345ed153ce
Damiano Salvi <salvi@intre.it>
2021-06-18 14:24:06 +0200

    step 2

--- src/main/java/it/intre/sal/kata/UglyCheckout.java
+++ src/main/java/it/intre/sal/kata/UglyCheckout.java
@@ -10,10 +10,9 @@ public class UglyCheckout implements Checkout {
     @Override
     public int pay(List<String> items, Map<String, Entry<Integer, Integer>> offers) {
         int res = 0;
-        int apples = 0;
-        int pears = 0;
-        int pineapples = 0;
-        int bananas = 0;
+
+        Map<String, Integer> itemCounts = new HashMap<>();
+
 
         Map<String, Integer> map = new HashMap<>();
         map.put("apple", 50);
@@ -22,31 +21,26 @@ public class UglyCheckout implements Checkout {
         map.put("banana", 60);
 
         for (String item : items) {
-            switch (item) {
-                case "apple":
-                    apples++;
-                    break;
-                case "pear":
-                    pears++;
-                    break;
-                case "pineapple":
-                    pineapples++;
-                    break;
-                case "banana":
-                    bananas++;
-                    break;
-            }
+            itemCounts.put(item, itemCounts.getOrDefault(item, 0) + 1);
         }
+
+
+        int apples = itemCounts.getOrDefault("apple", 0);
+        int pears = itemCounts.getOrDefault("pear", 0);
+        int pineapples = itemCounts.getOrDefault("pineapple", 0);
+        int bananas = itemCounts.getOrDefault("banana", 0);
 
         //Here I have to cycle through every offer to see if it applies
         for (Entry entry : offers.entrySet()) {
             switch (entry.getKey().toString()) {
                 case "apple":
                     int a1 = (int) ((Entry) entry.getValue()).getKey();
-                    if (apples >= a1) { res += (int) ((Entry) entry.getValue()).getValue(); }
+                    if (apples >= a1) {
+                        res += (int) ((Entry) entry.getValue()).getValue();
+                    }
                     apples -= a1;
                     break;
-                    //jb 2008-09-12: don't sell lychee anymore, but maybe in the future...
+                //jb 2008-09-12: don't sell lychee anymore, but maybe in the future...
 //                case "lychee":
 //                    int a2 = (int) ((Entry) entry.getValue()).getKey();
 //                    if (p >= a2) { res += (int) ((Entry) entry.getValue()).getValue(); }
@@ -54,17 +48,23 @@ public class UglyCheckout implements Checkout {
 //                    break;
                 case "pear":
                     int a2 = (int) ((Entry) entry.getValue()).getKey();
-                    if (pears >= a2) { res += (int) ((Entry) entry.getValue()).getValue(); }
+                    if (pears >= a2) {
+                        res += (int) ((Entry) entry.getValue()).getValue();
+                    }
                     pears -= a2;
                     break;
                 case "pineapple":
                     int a3 = (int) ((Entry) entry.getValue()).getKey();
-                    if (pineapples >= a3) { res += (int) ((Entry) entry.getValue()).getValue(); }
+                    if (pineapples >= a3) {
+                        res += (int) ((Entry) entry.getValue()).getValue();
+                    }
                     pineapples -= a3;
                     break;
                 case "banana":
                     int a4 = (int) ((Entry) entry.getValue()).getKey();
-                    if (bananas >= a4) { res += (int) ((Entry) entry.getValue()).getValue(); }
+                    if (bananas >= a4) {
+                        res += (int) ((Entry) entry.getValue()).getValue();
+                    }
                     bananas -= a4;
                     break;
             }
